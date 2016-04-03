6a5e4a54b9fcf691ecf583d1681a511b34a096e4
zapjx@hotmail.com <zapjx@hotmail.com>
2016-04-03 15:46:37 +0800

    sdfdf

---
+++ Mail/ChinaUnion_Agent/UserManagement/frmUserGroupImport.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ChinaUnion_Agent.UserManagement
+{
+    partial class frmUserGroupImport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.splitter1 = new BSE.Windows.Forms.Splitter();
+            this.panel1 = new BSE.Windows.Forms.Panel();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtAgent = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgAgent = new System.Windows.Forms.DataGridView();
+            this.txtGroupName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(735, 11);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 31);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "关闭";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.Color.Transparent;
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 58);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(1016, 3);
+            this.splitter1.TabIndex = 10;
+            this.splitter1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AssociatedSplitter = null;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.CaptionFont = new System.Drawing.Font("SimSun", 11.75F, System.Drawing.FontStyle.Bold);
+            this.panel1.CaptionHeight = 27;
+            this.panel1.Controls.Add(this.txtGroupName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnDownload);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnImport);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Controls.Add(this.txtAgent);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.CustomColors.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(160)))), ((int)(((byte)(160)))));
+            this.panel1.CustomColors.CaptionCloseIcon = System.Drawing.SystemColors.ControlText;
+            this.panel1.CustomColors.CaptionExpandIcon = System.Drawing.SystemColors.ControlText;
+            this.panel1.CustomColors.CaptionGradientBegin = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(244)))), ((int)(((byte)(242)))));
+            this.panel1.CustomColors.CaptionGradientEnd = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.CustomColors.CaptionGradientMiddle = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(232)))), ((int)(((byte)(228)))));
+            this.panel1.CustomColors.CaptionSelectedGradientBegin = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(189)))), ((int)(((byte)(210)))));
+            this.panel1.CustomColors.CaptionSelectedGradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(189)))), ((int)(((byte)(210)))));
+            this.panel1.CustomColors.CaptionText = System.Drawing.SystemColors.ControlText;
+            this.panel1.CustomColors.CollapsedCaptionText = System.Drawing.SystemColors.ControlText;
+            this.panel1.CustomColors.ContentGradientBegin = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.CustomColors.ContentGradientEnd = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(245)))), ((int)(((byte)(244)))));
+            this.panel1.CustomColors.InnerBorderColor = System.Drawing.SystemColors.Window;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Image = null;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(27, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.ShowCaptionbar = false;
+            this.panel1.Size = new System.Drawing.Size(1016, 58);
+            this.panel1.TabIndex = 9;
+            this.panel1.Text = "代理商导入";
+            this.panel1.ToolTipTextCloseIcon = null;
+            this.panel1.ToolTipTextExpandIconPanelCollapsed = null;
+            this.panel1.ToolTipTextExpandIconPanelExpanded = null;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDownload.Location = new System.Drawing.Point(650, 11);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 31);
+            this.btnDownload.TabIndex = 14;
+            this.btnDownload.Text = "模板下载";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Enabled = false;
+            this.btnImport.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnImport.Location = new System.Drawing.Point(560, 11);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 31);
+            this.btnImport.TabIndex = 9;
+            this.btnImport.Text = "上传";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSelect.Location = new System.Drawing.Point(478, 13);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(55, 28);
+            this.btnSelect.TabIndex = 7;
+            this.btnSelect.Text = "...";
+            this.btnSelect.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtAgent
+            // 
+            this.txtAgent.Location = new System.Drawing.Point(277, 19);
+            this.txtAgent.Name = "txtAgent";
+            this.txtAgent.Size = new System.Drawing.Size(198, 21);
+            this.txtAgent.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "文件地址：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgAgent);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1016, 680);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "代理商信息";
+            // 
+            // dgAgent
+            // 
+            this.dgAgent.AllowUserToAddRows = false;
+            this.dgAgent.AllowUserToDeleteRows = false;
+            this.dgAgent.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(233)))), ((int)(((byte)(207)))));
+            this.dgAgent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAgent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgAgent.Location = new System.Drawing.Point(3, 17);
+            this.dgAgent.Name = "dgAgent";
+            this.dgAgent.ReadOnly = true;
+            this.dgAgent.RowHeadersWidth = 10;
+            this.dgAgent.RowTemplate.Height = 23;
+            this.dgAgent.Size = new System.Drawing.Size(1010, 660);
+            this.dgAgent.TabIndex = 7;
+            // 
+            // txtGroupName
+            // 
+            this.txtGroupName.Location = new System.Drawing.Point(42, 17);
+            this.txtGroupName.Name = "txtGroupName";
+            this.txtGroupName.Size = new System.Drawing.Size(161, 21);
+            this.txtGroupName.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "组名：";
+            // 
+            // frmUserGroupImport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(1016, 741);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmUserGroupImport";
+            this.Text = "代理商导入";
+            this.Load += new System.EventHandler(this.frmInvoiceImport_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private BSE.Windows.Forms.Splitter splitter1;
+        private BSE.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtAgent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgAgent;
+        private System.Windows.Forms.TextBox txtGroupName;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Mail/ChinaUnion_Agent/UserManagement/frmUserGroupImport.cs
@@ -0,0 +1,188 @@
+﻿using ChinaUnion_Agent.Properties;
+using ChinaUnion_Agent.Wechat;
+using ChinaUnion_BO;
+using ChinaUnion_DataAccess;
+using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChinaUnion_Agent.UserManagement
+{
+    public partial class frmUserGroupImport : frmBase
+    {
+        public frmUserGroupImport()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+            // openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "Excel(*.xlsx)|*.xlsx|Excel 2000-2003(*.xls)|*.xls|CSV(*.csv)|*.csv|所有文件(*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                string FileName = openFileDialog.FileName;
+
+                var execelfile = new ExcelQueryFactory(FileName);
+
+                this.txtAgent.Text = FileName;
+                this.txtAgent.Enabled = false;
+
+                List<string> sheetNames = execelfile.GetWorksheetNames().ToList();
+
+
+
+
+                //代理商信息
+                List<Row> agentList = execelfile.Worksheet(0).ToList(); ;
+
+                if (agentList != null && agentList.Count > 0)
+                {
+
+                    this.btnImport.Enabled = true;
+                    dgAgent.Rows.Clear();
+                    dgAgent.Columns.Clear();
+                    foreach (String coloumn in agentList[0].ColumnNames)
+                    {
+                        this.dgAgent.Columns.Add(coloumn, coloumn);
+                    }
+
+                   // this.dgAgent.Columns.Add("result", "导入结果");
+
+                    for (int i = 0; i < agentList.Count; i++)
+                    {
+                        if (String.IsNullOrEmpty(agentList[i][0].Value.ToString().Trim()))
+                        {
+                            continue;
+                        }
+                        dgAgent.Rows.Add();
+                        DataGridViewRow row = dgAgent.Rows[i];
+                        foreach (String coloumn in agentList[0].ColumnNames)
+                        {
+                            row.Cells[coloumn].Value = agentList[i][coloumn];
+                        }
+
+                    }
+                }
+
+
+
+
+                dgAgent.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                dgAgent.AutoResizeColumns();
+
+                Cursor.Current = Cursors.Default;
+            }
+        }
+        BackgroundWorker worker; 
+        private void frmInvoiceImport_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.txtGroupName.Text.Trim()))
+            {
+                MessageBox.Show("请输入组名");
+                txtGroupName.Focus();
+                return;
+            }
+
+            //异步执行开始
+            worker.RunWorkerAsync();
+            frmProgress frm = new frmProgress(this.worker);
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.ShowDialog(this);
+            frm.Close();
+            this.btnImport.Enabled = false;
+        }
+        /// <summary>
+        /// 异步 开始事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //需要执行的代码
+
+            worker.ReportProgress(3, "开始导入信息...\r\n");
+            //导入代理商
+            GroupDao groupDao = new GroupDao();
+            Group group = new Group();
+            group.groupName = this.txtGroupName.Text.Trim();
+            group.description = group.groupName;
+            groupDao.Delete(group);
+            groupDao.Add(group);
+
+
+            UserDefinedGroupDao userDefinedGroupDao = new UserDefinedGroupDao();
+            for (int i = 0; i < dgAgent.RowCount; i++)
+            {
+                UserDefinedGroup userDefinedGroup = new UserDefinedGroup();
+                userDefinedGroup.groupName = group.groupName;
+                userDefinedGroup.member = dgAgent[0, i].Value.ToString();
+                userDefinedGroup.type = "代理商/渠道";
+                userDefinedGroupDao.Add(userDefinedGroup);
+                worker.ReportProgress(4, "导入" + userDefinedGroup.member + "信息完成...\r\n");
+               
+
+
+            }
+            //dgInvoice.AutoResizeColumns();
+            
+            worker.ReportProgress(5, "导入信息完成...\r\n");
+
+
+
+            //MessageBox.Show("数据上传完毕");
+
+        }
+
+
+        /// <summary>
+        /// 事件: 异步执行完成后 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("数据上传完毕。", "数据上传", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel格式|*.xlsx";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.FileName = "ImportUserDefinedGroup_Template.xlsx";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                String filePath = Application.StartupPath + @"\Template\ImportUserDefinedGroup_Template.xlsx";
+                File.Copy(filePath, saveFileDialog.FileName, true);
+            }
+        }
+    }
+}
